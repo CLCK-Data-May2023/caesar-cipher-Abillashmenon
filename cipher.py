357528d013a99afb88d464edc2733cbd87078db0
Abillash Mukundan <abillashmenon@gmail.com>
2023-08-06 22:41:28 -0400

    commit -m 'Finaloutput'

--- cipher.py
+++ cipher.py
@@ -1,1 +1,21 @@
 # add your code here
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+msg = input('Please enter a sentence: ')
+shift = int(input("What shift is to be used? "))
+encymsg = ''
+#Please enter a senctence: python is fun!
+#The encrypted sentence is: udymts nx kzs!
+for i in msg:
+    if i in letters:
+        filter1 = letters.find(i)
+        calc = (filter1 + shift) % 26
+        filter2 = letters[calc]
+        encymsg += filter2
+    else:
+        encymsg += i
+print('The encryoted message is: ' + encymsg )
+
+
+
+
